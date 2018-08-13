bbf4254045c8ea34eb7e8b80a747d8980f5f24e6
AixinLin <imlinaixin@gmail.com>
2018-08-12 23:05:21 -0400

    push

---
+++ llv2/收藏、已发/draftLike.swift
@@ -0,0 +1,64 @@
+//
+//  draftLike.swift
+//  llv2
+//
+//  Created by 林蔼欣 on 2018-08-12.
+//  Copyright © 2018 Luna Cao. All rights reserved.
+//
+
+import UIKit
+
+class draftLike: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    lazy var subViewControllers:[UIViewController] = {
+        return [ UIStoryboard(name:"Main", bundle: nil).instantiateViewController(withIdentifier: "vc1") as! vc1, UIStoryboard(name:"Main", bundle: nil).instantiateViewController(withIdentifier: "xianzhi") as! xianzhi, UIStoryboard(name:"Main", bundle: nil).instantiateViewController(withIdentifier: "exchange") as! exchange]
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        // Do any additional setup after loading the view.
+        
+        setViewControllers([subViewControllers[0]], direction: .forward, animated: true, completion: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return subViewControllers.count
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex:Int = subViewControllers.index(of: viewController) ?? 0
+        
+        if currentIndex <= 0 {
+            return nil
+        }
+        return subViewControllers[currentIndex - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let currentIndex:Int = subViewControllers.index(of: viewController) ?? 0
+        
+        if currentIndex >= (subViewControllers.count - 1) {
+            return nil
+        }
+        return subViewControllers[currentIndex + 1]
+    }
+    
+    
+    @IBAction func goback(_ sender: UIButton) {
+        // self.navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil);
+    }
+    
+    
+}
