2af9fc3ce055300ce105826cab268762bf7dbed5
AixinLin <imlinaixin@gmail.com>
2018-08-18 23:06:11 -0400

    m

--- llv2/cache.swift
+++ llv2/cache.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-
+//
+//func clearCache() {
+//
+//    // 取出cache文件夹目录 缓存文件都在这个目录下
+//    let cachePath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first
+//
+//    // 取出文件夹下所有文件数组
+//    let fileArr = FileManager.default.subpaths(atPath: cachePath!)
+//
+//    // 遍历删除
+//    for file in fileArr! {
+//
+//        let path = cachePath?.stringByAppendingString("/\(file)")
+//        if FileManager.defaultManager().fileExistsAtPath(path!) {
+//
+//            do {
+//                try FileManager.defaultManager().removeItemAtPath(path!)
+//            } catch {
+//
+//            }
+//        }
+//    }
+//}
 
