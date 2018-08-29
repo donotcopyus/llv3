bede86ebe28a76557790f8707236047e9d551cbe
crazydiamond <lunacao214@gmail.com>
2018-08-29 03:59:20 -0400

    search完成

--- llv2/搜索/exchangeSearch.swift
+++ llv2/搜索/exchangeSearch.swift
@@ -9,15 +9,13 @@
 import UIKit
 
 class exchangeSearch: UITableViewController {
+    
+    var pidSearchData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        print(pidSearchData.count)
     }
 
     override func didReceiveMemoryWarning() {
