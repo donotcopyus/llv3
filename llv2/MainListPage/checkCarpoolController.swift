c94dc40d618ff873b20ff38ad0ea1cb94db6eafb
AixinLin <imlinaixin@gmail.com>
2018-09-03 00:07:56 -0400

    头像改圆

--- llv2/MainListPage/checkCarpoolController.swift
+++ llv2/MainListPage/checkCarpoolController.swift
@@ -39,8 +39,8 @@ class checkCarpoolController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        headImage.layer.cornerRadius = headImage.frame.height / 2.0
-//        headImage.layer.masksToBounds = true
+        headImage.layer.cornerRadius = headImage.frame.height / 2.0
+        headImage.layer.masksToBounds = true
         //get pid，藏起来
         self.pidLabel.isHidden = true
         self.pidLabel.text = pid
--- llv2/搜索/carpoolSearchVC.swift
+++ llv2/搜索/carpoolSearchVC.swift
@@ -91,7 +91,7 @@ class carpoolSearchVC: UIViewController {
         super.viewDidLoad()
         
         
-        button = dropDownBtn.init(frame: CGRect(x:70, y:205, width: 150, height: 30))
+        button = dropDownBtn.init(frame: CGRect(x:50, y:205, width: 150, height: 30))
         
         button.setTitle("出发城市", for: .normal)
         
@@ -105,7 +105,7 @@ class carpoolSearchVC: UIViewController {
         
         
         //---------------------------------
-        b2 = dropDownBtn.init(frame: CGRect(x:225, y:205, width: 150, height: 30))
+        b2 = dropDownBtn.init(frame: CGRect(x:205, y:205, width: 150, height: 30))
         
         b2.setTitle("到达城市", for: .normal)
         
@@ -116,7 +116,7 @@ class carpoolSearchVC: UIViewController {
         self.view.addSubview(b2)
         
         ///---------------------------------------
-        b3 = dropDownBtn.init(frame: CGRect(x:70, y:140, width: 200, height: 30))
+        b3 = dropDownBtn.init(frame: CGRect(x:50, y:140, width: 200, height: 30))
         
         b3.setTitle("排列方式", for: .normal)
         
