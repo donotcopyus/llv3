413212f4020486a84c8dbe9ead6ae6435652e688
crazydiamond <lunacao214@gmail.com>
2018-09-18 11:21:34 -0400

    change

--- llv2/ratedonation.swift
+++ llv2/ratedonation.swift
@@ -59,16 +59,5 @@ class ratedonation: UIViewController {
         self.present(alert, animated: true, completion: nil)
         
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
