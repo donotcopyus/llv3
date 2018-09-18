922e6703c9a978e9ba2190701bc50be622b55908
crazydiamond <lunacao214@gmail.com>
2018-09-18 13:53:39 -0400

    push

--- llv2/donationVC.swift
+++ llv2/donationVC.swift
@@ -7,15 +7,82 @@
 //
 
 import UIKit
+import StoreKit
 
-class donationVC: UIViewController {
+class donationVC: UIViewController,SKProductsRequestDelegate,SKPaymentTransactionObserver {
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+
+        
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions{
+            switch(transaction.transactionState){
+            case .purchasing:
+                transactionPurchasing(transaction)
+            case .purchased:
+                transactionPurchased(transaction)
+            case .failed:
+                transactionFailed(transaction)
+            case .restored:
+                return
+            case .deferred:
+                return
+            }
+        }
+        
+    }
+    
+    fileprivate func requestProductInfo (_ productId: String){
+        let identifiers: Set<String> = [productId]
+        let request = SKProductsRequest(productIdentifiers: identifiers)
+        
+        request.delegate = self
+        request.start()
+    }
+    
+    fileprivate func transactionPurchasing(_ transaction: SKPaymentTransaction){
+        //交易中
+    }
+    
+    fileprivate func transactionPurchased(_ transaction:SKPaymentTransaction){
+        
+        if let receiptUrl = Bundle.main.appStoreReceiptURL{
+            let receipt = NSData(contentsOf: receiptUrl)
+            let receiptStr = receipt?.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: 3)){
+                print("receiptStr:\(String(describing:receiptStr))")
+                print("applicationUsername:\(String(describing:transaction.payment.applicationUsername))")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }
+        }
+        
+    }
+    
+    fileprivate func transactionFailed(_ transacation: SKPaymentTransaction){
+        let alert = UIAlertController(title: self.title, message: "付款出错", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "返回重试", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        } ))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        SKPaymentQueue.default().add(self as SKPaymentTransactionObserver)
     }
 
+    deinit{
+        SKPaymentQueue.default().remove(self as SKPaymentTransactionObserver)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -26,7 +93,9 @@ class donationVC: UIViewController {
  
     @IBAction func click(_ sender: UIButton) {
         
-        
+        if SKPaymentQueue.canMakePayments(){
+          //  requestProductInfo(<#T##productId: String##String#>)
+        }
         
     }
 
