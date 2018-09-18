5bc303b267dffff63ddb041ab39197351c741395
AixinLin <imlinaixin@gmail.com>
2018-09-18 16:35:19 -0400

    dd

--- llv2/sendAd/friendADVC.swift
+++ llv2/sendAd/friendADVC.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
+import ESPullToRefresh
 
-class friendADVC: UITableViewController {
 
+
+class friendADVC: UIViewController {
+
+    var b2 = dropDownBtn()
+    var b3 = dropDownBtn()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        b2 = dropDownBtn.init(frame: CGRect(x:210, y:90, width: 150, height: 40))
+        
+        b2.setTitle("选择时间", for: .normal)
+        
+        b2.translatesAutoresizingMaskIntoConstraints = true
+        
+        b2.dropView.dropDownOptions = ["Weldon Lib","丰盛附近","mason附近","kipps Ln","伦敦DT","Toronto","London","Hamilton","Waterloo","其他","任意"]
+        
+        self.view.addSubview(b2)
+        
+        
+        b3 = dropDownBtn.init(frame: CGRect(x:210, y:90, width: 150, height: 40))
+        
+        b3.setTitle("活动地址", for: .normal)
+        
+        b3.translatesAutoresizingMaskIntoConstraints = true
+        
+        b3.dropView.dropDownOptions = ["Weldon Lib","丰盛附近","mason附近","kipps Ln","伦敦DT","Toronto","London","Hamilton","Waterloo","其他","任意"]
+        
+        self.view.addSubview(b3)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,69 +51,6 @@ class friendADVC: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
